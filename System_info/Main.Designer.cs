9aac7b80090a79eaa1aace28bfd20af7308815b0
Mix Abcprintf <wh.cprintf@gmail.com>
2018-09-30 11:38:17 +0700

    update

--- System_info/Main.Designer.cs
+++ System_info/Main.Designer.cs
@@ -65,7 +65,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(293, 169);
+            this.button1.Location = new System.Drawing.Point(293, 306);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 23);
             this.button1.TabIndex = 0;
@@ -75,7 +75,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(413, 169);
+            this.button2.Location = new System.Drawing.Point(413, 306);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(114, 23);
             this.button2.TabIndex = 1;
@@ -103,7 +103,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(768, 220);
+            this.ClientSize = new System.Drawing.Size(768, 409);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
